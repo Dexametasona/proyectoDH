740d79788fcbe24ea61b11e1d29657fbf8e6063d
Enrique <devenriq@gmail.com>
2024-10-22 21:32:40 -0500

    START the frontend project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # proyectoDH
+
+## Client
+
+To run the project use the next code:
+
+```
+npm run dev
+```
+
+## Server
---
+++ client/context/AppContext.tsx
@@ -0,0 +1,51 @@
+import {
+  ReactNode,
+  useContext,
+  createContext,
+  useState,
+  useEffect,
+} from "react";
+
+interface AppContextType {
+  windowWidth: number;
+  isSmallScreen: boolean;
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+export const AppContextProvider: React.FC<{
+  children: ReactNode;
+}> = ({ children }) => {
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isSmallScreen = windowWidth < 640;
+
+  return (
+    <AppContext.Provider value={{ isSmallScreen, windowWidth }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error(
+      "useAppContext must be within an AppContextProvider context",
+    );
+  }
+
+  return context;
+};
