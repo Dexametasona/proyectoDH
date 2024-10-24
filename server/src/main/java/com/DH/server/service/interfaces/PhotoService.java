b23b7732120bd0d87808e64a7b17162f44c364df
Cristian <crismonsalve4@gmail.com>
2024-10-24 16:29:49 -0500

    feat(interfaces): PhotoService added

---
+++ server/src/main/java/com/DH/server/service/interfaces/PhotoService.java
@@ -0,0 +1,10 @@
+package com.DH.server.service.interfaces;
+
+import com.DH.server.model.entity.Photo;
+import com.DH.server.model.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface PhotoService extends GenericService<Photo> {
+//  Page<Product> getAll(Pageable page);
+}
