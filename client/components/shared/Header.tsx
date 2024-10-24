2a6c3dbfb4eb2a2285989dca23b300ebcb3ff5c5
Enrique <devenriq@gmail.com>
2024-10-24 18:40:57 -0500

    ADD navbar component

---
+++ client/components/shared/Header.tsx
@@ -0,0 +1,24 @@
+import { navbarOptions } from "@/types";
+import { Menu, User } from "lucide-react";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="h-20 bg-[#d9d9d9] w-full p-4 rounded-b-3xl flex justify-between items-center sm:rounded-b-none">
+      <div className="bg-[#8e8e93] rounded-full flex items-center justify-center p-2 sm:hidden">
+        <Menu />
+      </div>
+      <p className="uppercase text-center">GameYard</p>
+      <div className="gap-8 hidden sm:flex cursor-pointer">
+        {navbarOptions.map((option) => (
+          <p key={option.name}> {option.name} </p>
+        ))}
+      </div>
+      <div className="bg-[#8e8e93] rounded-full flex items-center justify-center p-2">
+        <User />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
