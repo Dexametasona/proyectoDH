d8f60474251ee61267b77486cdcbacce259eef83
Cristian <crismonsalve4@gmail.com>
2024-10-24 16:11:55 -0500

    feat(photoRepository) Photo repository was added

---
+++ server/src/main/java/com/DH/server/persistance/PhotoRepository.java
@@ -0,0 +1,9 @@
+package com.DH.server.persistance;
+
+import com.DH.server.model.entity.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+}
