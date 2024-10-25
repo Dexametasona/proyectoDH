c28e38bba105ef020ba9973de7bd15eac73f96ea
Jorge Solis <jorgesolis0397@gmail.com>
2024-10-24 20:53:32 -0500

    feat(categoryService): Category Service was added

---
+++ server/src/main/java/com/DH/server/service/implement/CategoryServiceImpl.java
@@ -0,0 +1,44 @@
+package com.DH.server.service.implement;
+
+import com.DH.server.model.entity.Category;
+import com.DH.server.persistance.CategoryRepository;
+import com.DH.server.service.interfaces.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Category create(Category entity) {
+        return this.categoryRepository.save(entity);
+    }
+
+    @Override
+    public Category getById(Long id) {
+        return this.categoryRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public Category updateById(Long id, Category entity) {
+        var previous=this.getById(id);
+        previous.setName(entity.getName());
+        return this.categoryRepository.save(previous);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.getById(id);
+        this.categoryRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Category> getAll() {
+        return List.of();
+    }
+}
---
+++ server/src/main/java/com/DH/server/service/interfaces/CategoryService.java
@@ -0,0 +1,8 @@
+package com.DH.server.service.interfaces;
+
+import com.DH.server.model.entity.Category;
+import org.springframework.stereotype.Service;
+
+
+public interface CategoryService extends GenericService<Category> {
+}
