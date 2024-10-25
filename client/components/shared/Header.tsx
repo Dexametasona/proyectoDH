1088b69a371c9a905a9fabc8efc0f9509fecec7a
Enrique <devenriq@gmail.com>
2024-10-24 19:24:44 -0500

    ADD navbar dropdown

--- client/components/shared/Header.tsx
+++ client/components/shared/Header.tsx
@@ -1,12 +1,31 @@
-import { navbarOptions } from "@/types";
 import { Menu, User } from "lucide-react";
-import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { navbarOptions } from "@/types";
 
 const Header = () => {
   return (
     <div className="h-20 bg-[#d9d9d9] w-full p-4 rounded-b-3xl flex justify-between items-center sm:rounded-b-none">
       <div className="bg-[#8e8e93] rounded-full flex items-center justify-center p-2 sm:hidden">
-        <Menu />
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Menu />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            {navbarOptions.map((option) => (
+              <DropdownMenuItem key={option.name}>
+                {option.name}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
       <p className="uppercase text-center">GameYard</p>
       <div className="gap-8 hidden sm:flex cursor-pointer">
