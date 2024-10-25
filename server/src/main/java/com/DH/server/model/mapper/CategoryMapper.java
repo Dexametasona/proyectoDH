b8bf8353b02013ad608d490df5d746cb4d57c524
Jorge Solis <jorgesolis0397@gmail.com>
2024-10-24 22:04:43 -0500

    feat(Category Mapper) Category Mapper was added

---
+++ server/src/main/java/com/DH/server/model/mapper/CategoryMapper.java
@@ -0,0 +1,19 @@
+package com.DH.server.model.mapper;
+
+import com.DH.server.model.dto.request.CategoryReqDto;
+import com.DH.server.model.dto.response.CategoryResDto;
+import com.DH.server.model.entity.Category;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring", uses = {})
+public abstract class CategoryMapper {
+
+    public abstract Category toEntity(CategoryReqDto request);
+
+    public abstract CategoryResDto toResponse (Category entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id",ignore = true)
+    public abstract void update(@MappingTarget Category previous, Category current);
+
+}
