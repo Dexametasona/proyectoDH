710a89c6900f65c9423ddb2f23ed486b86ac51ae
Paula Cuadra <paula.cuadraf@gmail.com>
2024-10-30 19:31:38 -0300

    Agregando diseño al home

--- client/components/ProductsCards.tsx
+++ client/components/ProductsCards.tsx
@@ -12,18 +12,25 @@ const ProductsCards = ({
   return (
     <>
       {bgImage ? (
+      
         <div
-          className="w-96 h-60 bg-cover bg-center bg-no-repeat"
+          className="relative w-full h-48 bg-cover bg-center rounded-t-lg"
           style={{ backgroundImage: `url(${bgImage})` }}
         >
-          <p> {title} </p>
+           <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent"></div>
+          <p className="absolute bottom-4 left-4 text-white font-bold"> {title} </p>
         </div>
+      
       ) : (
-        <div>
-          <Image src={cardImage} alt="product image" width={160} height={176} />
-          <p> {title} </p>
-          <p> {price} </p>
-          <p> {review} </p>
+        <div className="mt-2 text-center mb-4 bg-white p-3 rounded-lg shadow-md">
+          <Image src={cardImage} alt="product image" width={160} height={176} className="mb-3 rounded-lg"/>
+          <div className="justify-items-start">
+          <p className="text-text-color font-semibold"> {title} </p>
+          <p className="text-primary font-bold text-lg"> ${price}/hora </p>
+          <div className="flex items-center justify-center mt-1 text-text-color text-sm">
+            <span>⭐ {review}</span>
+          </div>
+          </div>
         </div>
       )}
     </>
