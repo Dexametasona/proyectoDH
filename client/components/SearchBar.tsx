6db6027c886d9c020663df61e3a2192b1f393322
Enrique <devenriq@gmail.com>
2024-11-19 20:20:52 -0500

    UPDATE searchbar

--- client/components/SearchBar.tsx
+++ client/components/SearchBar.tsx
@@ -1,30 +1,75 @@
 "use client";
-import { Search } from "lucide-react";
+import { CalendarDays, Search } from "lucide-react";
 import { useState } from "react";
+
+import { Input } from "@/components/ui/input";
+import { Calendar } from "@/components/ui/calendar";
+import { Button } from "./ui/button";
 
 const SearchBar = () => {
   const [search, setSearch] = useState("");
+  const [openFromCalendar, setOpenFromCalendar] = useState(false);
+  const [openToCalendar, setOpenToCalendar] = useState(false);
+
+  const handleOpenFromCalendar = () => {
+    setOpenFromCalendar(!openFromCalendar);
+  };
+
+  const handleOpenToCalendar = () => {
+    setOpenToCalendar(!openToCalendar);
+  };
 
   return (
-    <div className="container mx-auto flex items-center border border-gray-300 rounded-full px-4 py-2 shadow-md max-w-screen-md">
-      {/* Icono de búsqueda */}
-      <Search size={20} />
-      
-      {/* Dropdown */}
-      <select className="border-none bg-transparent px-2 text-gray-700 focus:ring-0 mr-2">
-        <option>Todos</option>
-        <option>Opción 1</option>
-        <option>Opción 2</option>
-      </select>
+    <div className="bg-secondary p-4 mx-4 rounded-xl flex flex-col items-center gap-2 sm:flex-row sm:items-end sm:gap-3 sm:mx-6">
+      <div className="w-full flex flex-col gap-2 sm:gap-3">
+        <p className="text-primary font-medium">¿Qué estás buscando?</p>
 
-      {/* Input */}
-      <input
-        type="text"
-        className="flex-grow focus:outline-none bg-transparent placeholder-gray-500"
-        placeholder="¿Qué estás buscando?"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
+        <div className="relative w-full">
+          <Search className="absolute right-4 top-2 text-disabled" />
+
+          <Input
+            type="text"
+            className="flex-grow focus:outline-none placeholder-disabled w-full border-none border-gray-300 bg-background rounded-full"
+            placeholder="Ej.: Saltarín"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="flex w-full gap-2 sm:gap-3">
+        <div className="mx-auto relative flex flex-col items-start w-full max-w-72 sm:gap-3">
+          <p className="text-primary">Desde</p>
+          <div onClick={handleOpenFromCalendar} className="max-w-72 w-full">
+            <Input
+              placeholder="dd/mm/aaaa"
+              className="border-gray-300 rounded-full bg-white placeholder-disabled w-full"
+            />
+            <CalendarDays className="hidden sm:block absolute right-2 top-10 text-disabled" />
+          </div>
+          {openFromCalendar && (
+            <Calendar className="absolute bg-white top-20 z-50" />
+          )}
+        </div>
+
+        <div className="mx-auto relative flex flex-col items-start max-w-72 w-full sm:gap-3">
+          <p className="text-primary">Hasta</p>
+          <div onClick={handleOpenToCalendar} className="max-w-72 w-full">
+            <Input
+              placeholder="dd/mm/aaaa"
+              className="border-gray-300 rounded-full bg-white placeholder-disabled"
+            />
+            <CalendarDays className="hidden sm:block absolute right-2 top-10 text-disabled" />
+          </div>
+          {openToCalendar && (
+            <Calendar className="absolute bg-white top-20 z-50" />
+          )}
+        </div>
+      </div>
+
+      <Button className="rounded-full w-full sm:w-fit sm:text-lg py-3 px-4 sm:h-12">
+        Buscar
+      </Button>
     </div>
   );
 };
