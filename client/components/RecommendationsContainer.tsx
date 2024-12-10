70503464a94c575c38601d96fef85948e8f0ec49
Dexametasona <machicadogomezalexander@gmail.com>
2024-12-10 18:57:33 -0500

    fix: solving bugs

--- client/components/RecommendationsContainer.tsx
+++ client/components/RecommendationsContainer.tsx
@@ -9,6 +9,7 @@ import CustomPagination from "./shared/CustomPagination";
 import { useAppContext } from "@/context/AppContext";
 import ResultsCards from "@/components/ResultsCards";
 import { getTopProducts } from "@/services/productService";
+import { IProductShort } from "@/types/IProduct";
 
 const RecommendationsContainer = ({
   name,
@@ -16,7 +17,7 @@ const RecommendationsContainer = ({
 }: CardsContainerProps) => {
   const { resultsProductsList } = useAppContext();
 
-  const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<IProductShort[]>([]);
 
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10;
@@ -42,7 +43,7 @@ const RecommendationsContainer = ({
 
   return (
     <section className="flex flex-col gap-4  justify-center px-6 mt-12 place-content-evenly ">
-      <p className="text-primary text-4xl text-left font-extrabold">
+      <p className="text-primary text-2xl text-left font-extrabold">
         {resultsProductsList.length > 0 ? "Resultados" : name}
       </p>
 
