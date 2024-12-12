eba8284ef729487366b6f6d2641deb05d95d16f7
Cristian <crismonsalve4@gmail.com>
2024-12-11 19:59:15 -0500

    Agrego botón whats

---
+++ client/components/shared/WhatsappBtn.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React from "react";
+
+const WhatsappBtn = () => {
+  const handleWhatsAppClick = () => {
+    const phoneNumber = "+573196466110"; // Replace with your WhatsApp number
+    const message = "Hola equipo de GameYard!, Tengo una pregunta"; // Optional pre-filled message
+
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(whatsappUrl, "_blank");
+  };
+
+  return (
+    <button className="whatsapp-button" onClick={handleWhatsAppClick}>
+      <img src="assets\icons\whatsapp-logo.png" alt="Logo whatsapp" />
+    </button>
+  );
+};
+
+export default WhatsappBtn;
