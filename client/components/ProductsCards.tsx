c0f4739bc1fdc7a918937c56f83646d9965cb732
Dexametasona <machicadogomezalexander@gmail.com>
2024-12-16 16:48:14 -0500

    solving bugs in merge

--- client/components/ProductsCards.tsx
+++ client/components/ProductsCards.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 
 import { Product } from "@/types";
 
-const ProductsCards = ({ id,  photoUrl, name, price}: Product) => {
+const ProductsCards = ({ id, photoUrl, name, price }: Product) => {
   const router = useRouter();
 
   const handleRedirect = (page: string) => {
@@ -14,40 +14,28 @@ const ProductsCards = ({ id,  photoUrl, name, price}: Product) => {
 
   return (
     <>
-      {photoUrl ? (
+      <div className="rounded-lg shadow-lg">
         <div
-          className=" objetivo relative w-full h-48 bg-cover bg-center rounded-t-lg cursor-pointer"
-          style={{ backgroundImage: `url(${photoUrl})` }}
-          onClick={() => handleRedirect("category")}
+          className="text-center bg-white p-3 rounded-t-lg shadow-md"
+          onClick={() => handleRedirect("product")}
         >
-          <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent"></div>
-          <p className="absolute bottom-4 left-4 text-white font-bold">
-            {name}
-          </p>
-        </div>
-      ) : (
-        <div className="rounded-t-lg shadow-lg col-span-2 md:col-span-1">
-          <div
-            className="text-center bg-white p-3 rounded-t-lg shadow-md"
-            onClick={() => handleRedirect("product")}
-          >
-            <div className="flex justify-center item-center">
-              <Image
-                src={photoUrl || ""}
-                alt="product image"
-                width={400}
-                height={200}
-                className="mb-3 w-80 h-80 rounded-t-lg cursor-pointer object-cover"
-                layout="fixed"
-              />
-            </div>
+          <div className="flex justify-center item-center">
+            <Image
+              src={photoUrl || ""}
+              alt="product image"
+              width={400}
+              height={200}
+              className="mb-3 w-80 h-80 rounded-t-lg cursor-pointer object-cover"
+              layout="fixed"
+            />
+          </div>
 
-            <div className="justify-items-start">
-              <p className="text-text-color font-semibold"> {name} </p>
-              <p className="text-primary font-bold text-lg"> ${price}/dia </p>
-            </div>
+          <div className="justify-items-start">
+            <p className="text-text-color font-semibold"> {name} </p>
+            <p className="text-primary font-bold text-lg"> ${price}/dia </p>
           </div>
         </div>
+      </div>
     </>
   );
 };
